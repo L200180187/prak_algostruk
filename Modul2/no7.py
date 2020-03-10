e2712888164c7dcad8f92775a1be020a4afa1ff3
L200180187 <48157476+L200180187@users.noreply.github.com>
2020-03-10 09:42:09 -0700

    Create no7.py

---
+++ Modul2/no7.py
@@ -0,0 +1,8 @@
+# nomer7
+
+import no2_modul2        
+
+class MhsTIF(no2_modul2.Mahasiswa):    
+    """Class MhsTIF yang dibangun dari class Mahasiswa"""
+    def kataKanPy(self):
+        print('Python is cool.')
